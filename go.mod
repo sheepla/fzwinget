7ac6505c5fc2ae36dd697d6fe0ad1b10731f599f
sheepla <62412884+sheepla@users.noreply.github.com>
2023-01-29 03:15:54 +0900

    Implement install, show, open command

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/sheepla/fzwinget
 go 1.19
 
 require (
+	github.com/dustin/go-humanize v1.0.1
+	github.com/ktr0731/go-fuzzyfinder v0.7.0
 	github.com/sheepla/tdloader v0.0.0-20230121182946-32d78171203a
 	github.com/tidwall/gjson v1.14.4
 	github.com/urfave/cli/v3 v3.0.0-alpha2
@@ -10,8 +12,19 @@ require (
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.5.4 // indirect
+	github.com/ktr0731/go-ansisgr v0.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/term v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/sheepla/fzwinget/api"
+	"github.com/sheepla/fzwinget/ui"
 	cli "github.com/urfave/cli/v3"
 )
 
@@ -21,12 +27,19 @@ const (
 	searchParamCategoryName = "search parameters"
 )
 
+const (
+	wingetDefaultFileName = "winget.exe"
+)
+
 type exitCode int
 
 const (
 	exitCodeOK exitCode = iota
 	exitCodeErrArgs
 	exitCodeErrAPI
+	exitCodeErrFuzzyFinder
+	exitCodeErrWinget
+	exitCodeErrOpenURL
 )
 
 func (e exitCode) Int() int {
@@ -89,23 +102,37 @@ func initApp() *cli.App {
 			UsageText: "install [OPTIONS] QUERY...",
 			Flags:     searchParamFlags,
 			Action:    runInstallCommand,
+			Hidden:    false,
 		},
 		{
 			Name:      "show",
 			Aliases:   []string{"s", "view"},
 			Usage:     "find packages and show detailed informations",
-			UsageText: "show QUERY...",
+			UsageText: "show [OPTIONS] QUERY...",
 			Flags:     searchParamFlags,
 			Action:    runShowCommand,
+			Hidden:    false,
 		},
 		{
 			Name:      "open",
 			Aliases:   []string{"o"},
 			Usage:     "find packages and open the page of the selected software(s)",
-			UsageText: "open QUERY...",
+			UsageText: "open [OPTIONS] QUERY...",
 			Flags:     searchParamFlags,
+			Hidden:    false,
+			Action:    runOpenCommand,
 		},
 	}
+
+	//app.Flags = []cli.Flag{
+	//	&cli.StringFlag{
+	//		Name:        "winget",
+	//		Aliases:     []string{},
+	//		Usage:       "specify path to winget executable file",
+	//		DefaultText: "--winget path/to/winget.exe",
+	//		EnvVars:     []string{"WINGET"},
+	//	},
+	//}
 
 	app.Action = runRootCommand
 
@@ -113,35 +140,159 @@ func initApp() *cli.App {
 }
 
 func buildSearchParam(ctx *cli.Context) *api.SearchParam {
-	return &api.SearchParam{
-		ID:          ctx.String("id"),
-		Name:        ctx.String("name"),
-		Description: ctx.String("description"),
-		Publisher:   ctx.String("publisher"),
-		Tags:        ctx.String("tags"),
-	}
-
+	return api.NewSearchParam(
+		strings.Join(ctx.Args().Slice(), " "),
+		ctx.String("id"),
+		ctx.String("name"),
+		ctx.String("publisher"),
+		ctx.String("description"),
+		ctx.String("tags"),
+		50,
+	)
 }
 
 func runRootCommand(ctx *cli.Context) error {
 	if ctx.NArg() == 0 {
 		return cli.Exit("must require arguments", exitCodeErrArgs.Int())
 	}
-	return cli.Exit(fmt.Sprintf("unknown command: %v", ctx.Args().Slice()), exitCodeOK.Int())
+	return cli.Exit(fmt.Sprintf("unknown command: %v", ctx.Args().Slice()), exitCodeErrArgs.Int())
 }
 
 func runInstallCommand(ctx *cli.Context) error {
-	result, err := api.Search(buildSearchParam(ctx))
+	result, err := searchPackages(ctx)
 	if err != nil {
-		return cli.Exit(err, exitCodeErrAPI.Int())
+		return cli.Exit(err, exitCodeOK.Int())
 	}
 
-	fmt.Println(result)
+	selected, err := ui.FindPackage(*result)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return cli.Exit("", exitCodeOK.Int())
+		}
 
-	return nil
+		return cli.Exit(err, exitCodeErrFuzzyFinder.Int())
+	}
+
+	for _, idx := range selected {
+		cmd := newWingetInstallCommand(
+			ctx.App.Writer,
+			ctx.App.ErrWriter,
+			(*result)[idx].ID,
+		)
+
+		if err := cmd.Run(); err != nil {
+			return cli.Exit(err, exitCodeErrWinget.Int())
+		}
+	}
+
+	return cli.Exit("", exitCodeErrAPI.Int())
 }
 
 func runShowCommand(ctx *cli.Context) error {
+	result, err := searchPackages(ctx)
+	if err != nil {
+		return cli.Exit(err, exitCodeOK.Int())
+	}
 
-	return nil
+	selected, err := ui.FindPackage(*result)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return cli.Exit("", exitCodeOK.Int())
+		}
+
+		return cli.Exit(err, exitCodeErrFuzzyFinder.Int())
+	}
+
+	for _, idx := range selected {
+		cmd := newWingetShowCommand(
+			ctx.App.Writer,
+			ctx.App.ErrWriter,
+			(*result)[idx].ID,
+		)
+
+		if err := cmd.Run(); err != nil {
+			return cli.Exit(err, exitCodeErrWinget.Int())
+		}
+	}
+
+	return cli.Exit(err, exitCodeOK.Int())
+}
+
+func runOpenCommand(ctx *cli.Context) error {
+	result, err := searchPackages(ctx)
+	if err != nil {
+		return cli.Exit(err, exitCodeOK.Int())
+	}
+
+	selected, err := ui.FindPackage(*result)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return cli.Exit("", exitCodeOK.Int())
+		}
+
+		return cli.Exit(err, exitCodeErrFuzzyFinder.Int())
+	}
+
+	for _, idx := range selected {
+		cmd := newOpenURLCommand(
+			ctx.App.Writer,
+			ctx.App.ErrWriter,
+			(*result)[idx].Homepage,
+		)
+
+		if err := cmd.Run(); err != nil {
+			return cli.Exit(err, exitCodeErrOpenURL.Int())
+		}
+	}
+
+	return cli.Exit(err, exitCodeOK.Int())
+}
+
+//func runDebugCommand(ctx *cli.Context) error {
+//	param := buildSearchParam(ctx)
+//	fmt.Fprintf(ctx.App.Writer, "%v", param)
+//
+//	return nil
+//}
+
+func searchPackages(ctx *cli.Context) (*api.SearchResult, error) {
+	param := buildSearchParam(ctx)
+	result, err := api.Search(param)
+	if err != nil {
+		return nil, cli.Exit(err, exitCodeErrAPI.Int())
+	}
+
+	if len([]api.Package(*result)) == 0 {
+		return nil, errors.New("no results found")
+	}
+
+	return result, nil
+}
+
+func newWingetShowCommand(stdout, stderr io.Writer, id string) *exec.Cmd {
+	cmd := exec.Command("cmd.exe", "/q", "/k", "winget.exe", "show", "--id", id)
+	cmd.Stdout = bufio.NewWriter(stdout)
+	cmd.Stderr = bufio.NewWriter(stderr)
+
+	return cmd
+}
+
+func newWingetInstallCommand(stdout, stderr io.Writer, id string) *exec.Cmd {
+	cmd := exec.Command("cmd.exe", "/q", "/k", "winget.exe", "install", "--id", id)
+	cmd.Stdout = bufio.NewWriter(stdout)
+	cmd.Stderr = bufio.NewWriter(stderr)
+
+	return cmd
+}
+
+func newOpenURLCommand(stdout, stderr io.Writer, url string) *exec.Cmd {
+	cmd := exec.Command(
+		"cmd.exe", "/q", "/c",
+		"start", "/b", url,
+	)
+
+	cmd.Stdout = bufio.NewWriter(stdout)
+	cmd.Stderr = bufio.NewWriter(stderr)
+
+	return cmd
 }
